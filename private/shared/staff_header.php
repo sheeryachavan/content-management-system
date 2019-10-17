60f75aaec378c54defd4f8ab7b63bee2830242f2
sheeryachavan <shreesh.chavan@gmail.com>
2019-10-16 20:12:41 -0400

    path setup

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -1,18 +1,23 @@
-<? if(!isset($page_title)){$page_title = "Staff Area";} ?>
-<html lang="en">
+<?php
+  if(!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
 
-<head>
-    <title>GLobe Bank - <?php echo $page_title; ?></title>
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" </head> <body>
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
+  </head>
+
+  <body>
     <header>
-        <h1>GBI</h1>
-        <h1>Globe Bank coming soon</h1>
+      <h1>GBI Staff Area</h1>
     </header>
+
     <navigation>
-        <ul>
-            <li>
-                <a href="index.php">Menu</a>
-            </li>
-        </ul>
+      <ul>
+        <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
+      </ul>
     </navigation>
